e39d209bc566b566fa83715c4854098e804951cd
Ramil Mamedov <ramil.mamedov7991@gmail.com>
2017-05-11 16:45:24 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
  function ayTap(type) {
-            if (type == 'Yaz') {
-                return [3, 4, 5] + ' Yaz';
-            } else if (type == 'Yay') {
-                return [6, 7, 8] + ' Yay';
-            } else if (type == 'Payiz') {
-                return [9, 10, 11] + ' Payiz';
-            } else if (type == 'Qis') {
-                return [1, 2, 12] + ' Qis';
-            }
-        }
-        var result = ayTap('Yay');
-        if (result) {
-            console.log (result);
-        } else {
-            console.log ('Error 404 Not Found')
-        }
+     if (type == 'Yaz') {
+         return [3, 4, 5] + ' Yaz';
+     }
+     else if (type == 'Yay') {
+         return [6, 7, 8] + ' Yay';
+     }
+     else if (type == 'Payiz') {
+         return [9, 10, 11] + ' Payiz';
+     }
+     else if (type == 'Qis') {
+         return [1, 2, 12] + ' Qis';
+     }
+ }
+ var result = ayTap('Yay');
+ if (result) {
+     console.log(result);
+ }
+ else {
+     console.log('Error 404 Not Found')
+ }
